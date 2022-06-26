60208ad1edbcf93262ed343d189b44ac5f15b7bd
Jepoy Factoran <factoran20@gmail.com>
2022-06-26 17:08:57 +0800

    added subscribe to store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import store from "./store";
 
+const unsubscribe = store.subscribe(()=>{
+  console.log('store changed', store.getState());
+})
+
 store.dispatch({
   type: "bugAdded",
   payload: {
     description: "test bug",
   },
 });
+
+unsubscribe();
 
 store.dispatch({
   type: "bugRemoved",
