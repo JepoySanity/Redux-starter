edf3aa6e997c02de04cefb2263571675daf8141e
Jepoy Factoran <factoran20@gmail.com>
2022-06-26 17:18:40 +0800

    added actions

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
+import { bugAdded } from "./actions";
 import store from "./store";
-import * as actions from "./actionTypes"
 
 const unsubscribe = store.subscribe(()=>{
   console.log('store changed', store.getState());
 })
 
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: "test bug",
-  },
-});
+store.dispatch(bugAdded("test 1"));
 
 unsubscribe();
 
